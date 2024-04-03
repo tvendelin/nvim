ec74e9af959881af0d463eb41086bcb04f90819f
Toomas Vendelin <toomas.vendelin@ionos.com>
2024-04-03 09:16:03 +0200

    Add keymapping for triple quotes and backtics
    Signed-off-by: Toomas Vendelin <toomas.vendelin@ionos.com>

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -5,17 +5,28 @@ vim.keymap.set("n", "<leader>sv", ":source %<CR>")
 -- If a key does not remap, go to Insert mode,
 -- type <C-v>, then the key.
 -- Either the key or the code for it will show up.
+
+-- Pythonic docstring
+vim.keymap.set("i", '"""<CR>', '"""<CR>"""<C-o>O', { noremap = true })
+vim.keymap.set("i", "'''<CR>", "'''<CR>'''<C-o>O", { noremap = true })
+
+-- Markdown code
+vim.keymap.set("i", "```<CR>", "```<CR>```<C-o>O", { noremap = true })
+
 vim.keymap.set("i", '""', '""<left>', { noremap = true })
 vim.keymap.set("i", "''", "''<left>", { noremap = true })
+vim.keymap.set("i", "``", "``<left>", { noremap = true })
 vim.keymap.set("i", "(", "()<left>", { noremap = true })
 vim.keymap.set("i", "[", "[]<left>", { noremap = true })
 if jit.os == "OSX" then
 	vim.keymap.set("i", "{", "{}<left>", { noremap = true })
-	vim.keymap.set("i", "{<CR>", "{<CR><CR>}<up><TAB>", { noremap = true })
+	vim.keymap.set("i", "{<CR>", "{<CR>}<C-o>O", { noremap = true })
 else
 	vim.keymap.set("i", "<S-{>", "{}<left>", { noremap = true })
 	vim.keymap.set("i", "<S-{><CR>", "{<CR>}<C-o>O", { noremap = true })
 end
+
+-- Navigation between splits
 vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true })
