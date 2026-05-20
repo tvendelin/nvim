3712a003b5afcacb5302e70bd4b7ba6b632ecd23
Toomas Vendelin <toomas.vendelin@ionos.com>
2026-05-20 09:28:28 +0200

    Use my patch to dadbod untill merged upstream
    Signed-off-by: Toomas Vendelin <toomas.vendelin@ionos.com>

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,7 +1,7 @@
 return {
 	"kristijanhusak/vim-dadbod-ui",
 	dependencies = {
-		{ "tpope/vim-dadbod", lazy = true },
+		{ "tvendelin/vim-dadbod", branch="fix-duckdb-tables-220", lazy = true },
 		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
 	},
 	cmd = {
